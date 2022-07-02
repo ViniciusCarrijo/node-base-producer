917a17dadb2ad9a3e9be1a6813c72734e3fd04b0
Vinicius Carrijo <viniciuscarrijo@hotmail.com>
2022-07-02 11:36:04 -0300

    feat: add email queue

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,12 +8,19 @@ const getPing = async (_req, res) => {
 };
 
 const postLog = async (req, res) => {
-  const body = { message: req.body.message };
+  const body = { log: req.body.message };
   await queues.log.add(body);
+  return res.send(body).status(200);
+};
+
+const postEmail = async (req, res) => {
+  const body = { email: req.body.message };
+  await queues.email.add(body);
   return res.send(body).status(200);
 };
 
 router.get('/', getPing);
 router.post('/log', postLog);
+router.post('/email', postEmail);
 
 export default router;
